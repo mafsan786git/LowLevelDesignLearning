8a4b82bfeb37d2ffc3e264a94f3867eb5122fb28
Mohd. Afsan Ahmad <mafsan78692@gmail.com>
2024-10-08 11:13:47 +0530

    added LazyInitialisation for Singleton

---
+++ SingletonDesignPattern/src/LazyInitialisation.java
@@ -0,0 +1,29 @@
+/**
+ * This class implements the Singleton design pattern using lazy initialization.
+ * The Singleton pattern ensures that only one instance of a class is created and provides a global point of access to it.
+ */
+class LazyInitialisation {
+    /**
+     * The single instance of the LazyInitialisation class.
+     */
+    private static LazyInitialisation instance;
+
+    /**
+     * Private constructor to prevent instantiation from outside the class.
+     */
+    private LazyInitialisation() {
+        // Private constructor to prevent instantiation from outside the class
+    }
+    /**
+     * Returns the single instance of the LazyInitialisation class.
+     * If the instance is not yet created, it will be created and returned.
+     *
+     * @return The single instance of the LazyInitialisation class.
+     */
+    public static LazyInitialisation getInstance() {
+        if (instance == null) { 
+            instance = new LazyInitialisation(); 
+        }
+        return instance;
+    }
+}
